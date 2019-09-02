3d0b934245f6acc2bf43c52dd36e63b03d287430
heny <henrik.nystrom@episerver.com>
2019-09-02 17:12:57 +1200

    Add Asset methods for HttpResponseMessage
    Fixes: #7

---
+++ src/AngleAssert.Xunit/HtmlAsserts/HtmlResponseAssert.cs
@@ -0,0 +1,98 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using AngleAssert;
+using Xunit.Sdk;
+
+namespace Xunit
+{
+    public partial class Assert
+    {
+        private static async Task<string> GetHtml(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// Verifies that the HTML returned in a <see cref="HttpResponseMessage"/> is equivalent to the expected HTML using the default options.
+        /// </summary>
+        /// <param name="expected">The expected HTML document</param>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> containing the HTML to be compared against</param>
+        /// <exception cref="EqualException">Thrown when the HTML documents are not equivalent</exception>
+
+        public static async Task Html(string expected, HttpResponseMessage response)
+        {
+            GuardArgumentNotNull(nameof(expected), expected);
+            GuardArgumentNotNull(nameof(response), response);
+
+            Html(expected, await GetHtml(response));
+        }
+
+        /// <summary>
+        /// Verifies that the HTML returned in a <see cref="HttpResponseMessage"/> is equivalent to the expected HTML using the provided options.
+        /// </summary>
+        /// <param name="expected">The expected HTML document</param>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> containing the HTML to be compared against</param>
+        /// <param name="ignoreAdditionalAttributes">Indicates if additional attribute on any element in the candidate HTML should be ignored.</param>
+        /// <param name="ignoreAdditionalClassNames">Indicates if additional class names on any element in the candidate HTML should be ignored.</param>
+        /// <param name="ignoreClassNameOrder">Indicates if the order of class names in the candidate HTML should be ignored</param>
+        /// <exception cref="EqualException">Thrown when the HTML strings are not equivalent</exception>
+        public static async Task Html(string expected, HttpResponseMessage response, bool ignoreAdditionalAttributes = false, bool ignoreAdditionalClassNames = false, bool ignoreClassNameOrder = true)
+        {
+            GuardArgumentNotNull(nameof(expected), expected);
+            GuardArgumentNotNull(nameof(response), response);
+
+            Html(expected, await GetHtml(response), ignoreAdditionalAttributes, ignoreAdditionalClassNames, ignoreClassNameOrder);
+        }
+
+        /// <summary>
+        /// Verifies that a specific element in a HTML document returned in a <see cref="HttpResponseMessage"/>  is equivalent to a given fragment using the default options.
+        /// </summary>
+        /// <param name="expected">The expected HTML fragment element</param>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> containing the HTML to be compared against</param>
+        /// <param name="selector">A selector used to find the element to compare.</param>
+        /// <exception cref="HtmlException">Thrown when the HTML elements are not equivalent</exception>
+        public static async Task HtmlElement(string expected, HttpResponseMessage response, string selector)
+        {
+            GuardArgumentNotNull(nameof(expected), expected);
+            GuardArgumentNotNull(nameof(response), response);
+            GuardArgumentNotNull(nameof(selector), selector);
+
+            HtmlElement(expected, await GetHtml(response), selector);
+        }
+
+        /// <summary>
+        /// Verifies that a specific element in a HTML document returned in a <see cref="HttpResponseMessage"/>  is equivalent to a given fragment using the provided options.
+        /// </summary>
+        /// <param name="expected">The expected HTML fragment element</param>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> containing the HTML to be compared against</param>
+        /// <param name="selector">A selector used to find the element to compare.</param>
+        /// <param name="includeSelectedElement">Indicated if the selected element itself should be included in the comparison or if only it's content should be compared.</param>
+        /// <param name="elementSelectionMode">Indicates how selected elements should be compared.</param>
+        /// <param name="ignoreAdditionalAttributes">Indicates if additional attribute on any element in the candidate HTML should be ignored.</param>
+        /// <param name="ignoreAdditionalClassNames">Indicates if additional class names on any element in the candidate HTML should be ignored.</param>
+        /// <param name="ignoreClassNameOrder">Indicates if the order of class names in the candidate HTML should be ignored</param>
+        /// <exception cref="HtmlException">Thrown when the HTML elements are not equivalent</exception>
+        public static async Task HtmlElement(string expected, HttpResponseMessage response, string selector, bool includeSelectedElement = false, ElementSelectionMode elementSelectionMode = ElementSelectionMode.First, bool ignoreAdditionalAttributes = false, bool ignoreAdditionalClassNames = false, bool ignoreClassNameOrder = true)
+        {
+            GuardArgumentNotNull(nameof(expected), expected);
+            GuardArgumentNotNull(nameof(response), response);
+            GuardArgumentNotNull(nameof(selector), selector);
+
+            HtmlElement(expected, await GetHtml(response), selector, includeSelectedElement, elementSelectionMode, ignoreAdditionalAttributes, ignoreAdditionalClassNames, ignoreClassNameOrder);
+        }
+
+        /// <summary>
+        /// Checks if the HTML document returned in the <see cref="HttpResponseMessage"/> contains any element matched by the selector.
+        /// </summary>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> containing the HTML that should be asserted.</param>
+        /// <param name="selector">The selector used to find an element.</param>
+        public static async Task HtmlContains(HttpResponseMessage response, string selector)
+        {
+            GuardArgumentNotNull(nameof(response), response);
+            GuardArgumentNotNull(nameof(selector), selector);
+
+            HtmlContains(await GetHtml(response), selector);
+        }
+    }
+}
---
+++ test/AngleAssert.Test/HtmlResponseAssertTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace AngleAssert
+{
+    public class HtmlResponseAssertTest
+    {
+        [Fact]
+        public async Task Html_WhenHtmlIsIdentical_ShouldNotThrow()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("<p>text</p>") };
+
+            await Assert.Html("<p>text</p>", response);
+        }
+
+        [Fact]
+        public async Task Html_WhenHtmlIsDifferent_ShouldThrow()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("<div>two</div>") };
+
+            await Assert.ThrowsAsync<EqualException>(async () => await Assert.Html("<p>one</p>", response));
+        }
+
+        [Fact]
+        public async Task Html_WhenHtmlIsAcceptablyDifferent_ShouldNotThrow()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("<p class=\"one two\">text</p>") };
+
+            await Assert.Html("<p class=\"one\">text</p>", response, ignoreAdditionalClassNames: true);
+        }
+
+        [Fact]
+        public async Task HtmlElement_WhenHtmlElementIsNotFound_ShouldReportReason()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("<div><p>one</p></div>") };
+
+            var ex = await Assert.ThrowsAsync<HtmlException>(async () => await Assert.HtmlElement("one", response, "p.class"));
+
+            Assert.Equal(HtmlCompareMismatchReason.ElementNotFound, ex.Reason);
+        }
+
+        [Fact]
+        public async Task HtmlContains_WhenHtmlDoesNotContainElement_ShouldThrow()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("<p>one</p>") };
+
+            await Assert.ThrowsAsync<HtmlContainsException>(async () => await Assert.HtmlContains(response, "div"));
+        }
+    }
+}
